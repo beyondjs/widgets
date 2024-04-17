bcfbbe776d2b75cc8fe0b287c99c986b93a09057
Henry Box <14365005+henrybox@users.noreply.github.com>
2024-04-17 21:21:40 +0000

    feat: restructuring folders (/src), html-controller added, tests added

---
+++ tests/modules/html-controller/csr/index.ts
@@ -0,0 +1,25 @@
+import { WidgetClientController } from '@beyond-js/widgets/controller';
+import type { BeyondWidget } from '@beyond-js/widgets/render';
+
+export abstract class HTMLController extends WidgetClientController {
+	get widget(): BeyondWidget {
+		return <BeyondWidget>super.widget;
+	}
+
+	// Declare properties and lifecycle methods for the widget
+	mount(props?: Record<string, any>) {
+		const holder: HTMLSpanElement = (<any>this.widget).holder;
+		holder.innerHTML = this.html(props);
+
+		this.addListeners();
+	}
+
+	unmount() {
+		this.removeListeners();
+	}
+
+	abstract html(props: Record<string, any>): string;
+
+	abstract addListeners(): void;
+	abstract removeListeners(): void;
+}
---
+++ tests/modules/pages/page-1/index.ts
@@ -0,0 +1,9 @@
+import { HTMLController } from '@beyond-js/widgets-tests/html-controller';
+
+export /*bundle*/ class Controller extends HTMLController {
+	html(props: Record<string, any>) {
+		return `<div class="page">Page #1</div>`;
+	}
+
+	addListeners() {}
+}
